e18cd150ae8a070c1a932ab817297ea0b1d9725b
kryo <mail@fabiosousa.com>
2011-01-31 21:36:16 +0000

    WorldStateAlwaysUpFrame repositioned

--- Tukui/modules/achievement.lua
+++ Tukui/modules/achievement.lua
@@ -13,10 +13,11 @@ function DungeonCompletionAlertFrame_FixAnchors()
 	DungeonCompletionAlertFrame1:SetPoint("TOP", AchievementAlertFrame1, "BOTTOM", 0, -20)
 end
 
+WorldStateAlwaysUpFrame:ClearAllPoints()
+WorldStateAlwaysUpFrame:SetPoint("TOP", UIParent, "TOP", 0, TukuiDB.Scale(-45))
+
 --[[ and talent micro menu
 function TalentMicroButtonAlert_FixAnchors()
-	if (not TalentMicroButtonAlert) then return end
-	
 	TalentMicroButtonAlert:ClearAllPoints();
 	TalentMicroButtonAlert:SetPoint("BOTTOM", bottompanel, "TOP", 0, 0)
 end--]]
