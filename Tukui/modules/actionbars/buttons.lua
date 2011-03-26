0fee35fff6dbd8af6d8f368097395f00a1245f63
kryo <mail@fabiosousa.com>
2011-03-26 18:27:25 +0000

    didn't like the size of the buttons, much better now

--- Tukui/modules/actionbars/buttons.lua
+++ Tukui/modules/actionbars/buttons.lua
@@ -1,6 +1,8 @@
 local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
 -- This is the file for our action bars settings in game via mouseover buttons around action bars.
 
+local bs = (T.buttonsize*3)+(T.buttonspacing*2)
+
 local function ShowOrHideBar(bar, button)
 	local db = TukuiDataPerChar
 	if bar:IsShown() then
@@ -19,16 +21,18 @@ local function MoveButtonBar(button, bar)
 			button:ClearAllPoints()
 			button:Point("BOTTOM", TukuiBar3, "TOP", T.buttonspacing, 2)
 			button:SetBackdropBorderColor(.86,.07,.23)
+			button.text:Point("CENTER", 0, 3)
 			button.text:SetText("|cffDC143C-|r")
 		else
 			db.hidebar3 = true
 			button:ClearAllPoints()
 			if T.lowversion then
-				button:Point("BOTTOMLEFT", TukuiBar1, "TOPRIGHT", -((T.buttonsize*7)+(T.buttonspacing*5)), 2)
+				button:Point("BOTTOMLEFT", TukuiBar1, "TOPRIGHT", -((T.buttonsize*5)+(T.buttonspacing*2)), 2)
 			else
-				button:Point("BOTTOMLEFT", TukuiBar1, "TOPRIGHT", -((T.buttonsize*6)+(T.buttonspacing*4)), 2)
+				button:Point("BOTTOMLEFT", TukuiBar1, "TOPRIGHT", -((T.buttonsize*3)+T.buttonspacing), 2)
 			end
 			button:SetBackdropBorderColor(.4,.80,0)
+			button.text:Point("CENTER", 0, 1)
 			button.text:SetText("|cff66CD00+|r")
 		end
 	end
@@ -38,18 +42,19 @@ local function MoveButtonBar(button, bar)
 			db.hidebar5 = false
 			TukuiBar4Button:Hide()
 			button:ClearAllPoints()
-			button:Height(TukuiBar4:GetHeight())
+			button:Height(bs*2)
 			button:Point("RIGHT", UIParent, "RIGHT", -1, T.buttonspacing)
 			button:SetBackdropBorderColor(.86,.07,.23)
 			button.text:SetText("|cffDC143C-|r")
 			TukuiPetBar:SetPoint("RIGHT", TukuiBar5, "LEFT", -T.petbuttonsize, 0)
 		else
-			db.hidebar5 = true
+			db.hidebar5 = true -- DONE!
 			TukuiBar4Button:Show()
 			button:ClearAllPoints()
-			button:Height(TukuiBar4:GetHeight()/2)
-			button:Point("RIGHT", UIParent, "RIGHT", -1, ((T.buttonsize*3)+(T.buttonspacing*4)))
+			button:Height(bs)
+			button:Point("BOTTOMRIGHT", UIParent, "RIGHT", -1, T.buttonspacing)
 			button:SetBackdropBorderColor(.4,.80,0)
+			button.text:Point("CENTER", 1, 1)
 			button.text:SetText("|cff66CD00+|r")
 			button:Show()
 			TukuiPetBar:SetPoint("RIGHT", TukuiBar4, "LEFT", -T.petbuttonsize/2, 0)
@@ -60,8 +65,8 @@ local function MoveButtonBar(button, bar)
 		if bar:IsShown() then
 			db.hidebar4 = false
 			button:ClearAllPoints()
-			button:Height(TukuiBar4:GetHeight()/2)
-			button:Point("RIGHT", UIParent, "RIGHT", -1, -((T.buttonsize*3)+(T.buttonspacing*2)))
+			button:Height(bs)
+			button:Point("TOPRIGHT", UIParent, "RIGHT", -1, 0)
 			button:SetBackdropBorderColor(.86,.07,.23)
 			button.text:SetText("|cffDC143C-|r")
 			TukuiBar5Button:Show()
@@ -70,9 +75,10 @@ local function MoveButtonBar(button, bar)
 			db.hidebar4 = true
 			TukuiBar5Button:Hide()
 			button:ClearAllPoints()
-			button:Height(TukuiBar4:GetHeight())
+			button:Height(bs*2)
 			button:Point("RIGHT", UIParent, "RIGHT", -1, T.buttonspacing)
 			button:SetBackdropBorderColor(.4,.80,0)
+			button.text:Point("CENTER", 1, 1)
 			button.text:SetText("|cff66CD00+|r")
 			TukuiPetBar:SetPoint("RIGHT", UIParent, "RIGHT", -T.petbuttonsize/2, 0)
 		end
@@ -90,7 +96,7 @@ local function DrPepper(self, bar) -- guess what! :P
 end
 
 local TukuiBar3Button = CreateFrame("Button", "TukuiBar3Button", UIParent)
-TukuiBar3Button:Width(TukuiBar3:GetWidth())
+TukuiBar3Button:Width(bs*2)
 TukuiBar3Button:Height(12)
 TukuiBar3Button:Point("BOTTOM", TukuiBar3, "TOP", T.buttonspacing, 2)
 TukuiBar3Button:SetTemplate("Panels")
@@ -101,13 +107,13 @@ TukuiBar3Button:SetScript("OnClick", function(self) DrPepper(self, TukuiBar3) en
 TukuiBar3Button:SetScript("OnEnter", function(self) self:SetAlpha(1) end)
 TukuiBar3Button:SetScript("OnLeave", function(self) self:SetAlpha(0) end)
 TukuiBar3Button.text = T.SetFontString(TukuiBar3Button, C.media.uffont, 20)
-TukuiBar3Button.text:Point("CENTER", 1, 1)
+TukuiBar3Button.text:Point("CENTER", 0, 3)
 TukuiBar3Button.text:SetText("|cffDC143C-|r")
 
-local TukuiBar4Button = CreateFrame("Button", "TukuiBar4Button", UIParent)
+local TukuiBar4Button = CreateFrame("Button", "TukuiBar4Button", UIParent) -- DONE!
 TukuiBar4Button:Width(12)
-TukuiBar4Button:Height(TukuiBar4:GetHeight()/2)
-TukuiBar4Button:Point("RIGHT", UIParent, "RIGHT", -1, -((T.buttonsize*3)+(T.buttonspacing*2)))
+TukuiBar4Button:Height(bs)
+TukuiBar4Button:Point("TOPRIGHT", UIParent, "RIGHT", -1, 0)
 TukuiBar4Button:SetTemplate("Panels")
 TukuiBar4Button:RegisterForClicks("AnyUp")
 TukuiBar4Button:SetAlpha(0)
@@ -116,12 +122,12 @@ TukuiBar4Button:SetScript("OnClick", function(self) DrPepper(self, TukuiBar4) en
 TukuiBar4Button:SetScript("OnEnter", function(self) self:SetAlpha(1) TukuiBar5Button:SetAlpha(1) end)
 TukuiBar4Button:SetScript("OnLeave", function(self) self:SetAlpha(0) TukuiBar5Button:SetAlpha(0) end)
 TukuiBar4Button.text = T.SetFontString(TukuiBar4Button, C.media.uffont, 20)
-TukuiBar4Button.text:Point("CENTER", 1, 1)
+TukuiBar4Button.text:Point("CENTER", 0, 1)
 TukuiBar4Button.text:SetText("|cffDC143C-|r")
 
 local TukuiBar5Button = CreateFrame("Button", "TukuiBar5Button", UIParent)
 TukuiBar5Button:Width(12)
-TukuiBar5Button:Height(TukuiBar4:GetHeight())
+TukuiBar5Button:Height(bs*2)
 TukuiBar5Button:Point("RIGHT", UIParent, "RIGHT", -1, T.buttonspacing)
 TukuiBar5Button:SetTemplate("Panels")
 TukuiBar5Button:RegisterForClicks("AnyUp")
@@ -132,7 +138,7 @@ TukuiBar5Button:SetScript("OnClick", function(self) DrPepper(self, TukuiBar5) en
 TukuiBar5Button:SetScript("OnEnter", function(self) self:SetAlpha(1) TukuiBar4Button:SetAlpha(1) end)
 TukuiBar5Button:SetScript("OnLeave", function(self) self:SetAlpha(0) TukuiBar4Button:SetAlpha(0) end)
 TukuiBar5Button.text = T.SetFontString(TukuiBar5Button, C.media.uffont, 20)
-TukuiBar5Button.text:Point("CENTER", 1, 1)
+TukuiBar5Button.text:Point("CENTER", 0, 1)
 TukuiBar5Button.text:SetText("|cffDC143C-|r")
 
 -- exit vehicle button on left side of bottom action bar
