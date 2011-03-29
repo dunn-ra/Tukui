aec002ca5843f042f79cd40effee6b7ac2247a98
kryo <mail@fabiosousa.com>
2011-03-29 13:45:56 +0100

    meh

--- Tukui/modules/nameplates/nameplates.lua
+++ Tukui/modules/nameplates/nameplates.lua
@@ -343,8 +343,8 @@ local function UpdateObjects(frame)
 		frame.hp.level:SetText("B")
 		frame.hp.level:SetTextColor(0.8, 0.05, 0)
 		frame.hp.level:Show()
-	--elseif not elite and level == mylevel then
-		--frame.hp.level:Hide()
+	elseif not elite and level == mylevel then
+		frame.hp.level:Hide()
 	else
 		frame.hp.level:SetText(level..(elite and "+" or ""))
 		frame.hp.level:Show()
