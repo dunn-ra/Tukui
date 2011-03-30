7c262b0d1c8f9e93ed1637dd2dcf53bf440e9f4b
kryo <mail@fabiosousa.com>
2011-03-30 01:02:26 +0100

    armor dummy

--- Tukui/modules/blizzard/durability.lua
+++ Tukui/modules/blizzard/durability.lua
@@ -4,10 +4,6 @@ local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, vari
 hooksecurefunc(DurabilityFrame,"SetPoint",function(self,_,parent)
     if (parent == "MinimapCluster") or (parent == _G["MinimapCluster"]) then
         self:ClearAllPoints()
-		if C["actionbar"].bottomrows == true then
-			self:Point("BOTTOM", UIParent, "BOTTOM", 0, 228)
-		else
-			self:Point("BOTTOM", UIParent, "BOTTOM", 0, 200)
-		end
+		self:Point("BOTTOM", UIParent, "BOTTOM", 0, 275)
     end
 end)
