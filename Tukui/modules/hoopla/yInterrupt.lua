07ffb8a2f2f12b1170e78652172fbdba3d6fd167
kryo <mail@fabiosousa.com>
2011-04-27 16:09:39 +0100

    fix for the interrupt script

--- Tukui/modules/hoopla/yInterrupt.lua
+++ Tukui/modules/hoopla/yInterrupt.lua
@@ -5,7 +5,7 @@ if C["misc"].interrupt ~= true then return end
 local function OnEvent(self, event, ...)
 	if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
 		if GetNumRaidMembers() > 5 then channel = "RAID" elseif GetNumPartyMembers() > 0 then channel = "PARTY" else return end
-		local _, eventType, _, sourceName, _, _, destName, _, spellID, spellName, _, extraskillID, extraskillname = ...
+		local _, eventType, _, _, sourceName, _, _, destName, _, spellID, spellName, _, extraskillID, extraskillname = ...
 		if (eventType == "SPELL_INTERRUPT") and sourceName == T.myname then
 			SendChatMessage(GetSpellLink(extraskillID).." interrupted", channel)
 		elseif (eventType == "SPELL_STOLEN") and sourceName == T.myname then
