0cbb8b69ad7da26f65b34532dba9d3f5ca88dde7
kryo <mail@fabiosousa.com>
2011-06-23 20:41:56 +0100

    4.2 updates

--- Tukui/modules/hoopla/yAnnounce.lua
+++ Tukui/modules/hoopla/yAnnounce.lua
@@ -3,64 +3,57 @@ local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, vari
 if C.misc.announce ~= true then return end
 
 local function Update(self, event, ...)
-local _, eventType, _, _, sourceName, _, _, destName, _, spellID, spellName, _, extraskillID, extraskillname = ...
+if T.toc < 40200 then
+	timestamp, eventType, _, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, spellID, spellName, _, extraskillID, extraskillname = ...
+else
+	timestamp, eventType, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellID, spellName, _, extraskillID, extraskillname = ...
+end
+
+if GetNumRaidMembers() >= 5 then channel = "RAID" elseif GetNumPartyMembers() >= 1 then channel = "PARTY" else return end
+--local channel = "SAY"
+	
 	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-		if sourceName == T.myname or sourceName == T.mypet then
-			if GetNumRaidMembers() >= 5 then channel = "RAID" elseif GetNumPartyMembers() >= 1 then channel = "PARTY" else return end
-			--local channel = "SAY"
-			if eventType == "SPELL_AURA_APPLIED" then -- CCs
-				-- Druid
+		if sourceName == T.myname then
+			if eventType == "SPELL_AURA_APPLIED" then
 				if spellID == 2637 or spellID == 33786 or spellID == 339 then -- Hibernate, Cyclone, Roots
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Hunter
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				elseif spellID == 3355 or spellID == 19386 then -- Freezing Trap, Wyvern Sting
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Mage
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				elseif spellID == 118 or spellID == 61305 or spellID == 28272 or spellID == 61721 or spellID == 61780 or spellID == 28271 then -- Polymorph
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Paladin
-				elseif spellID == 10326 or spellID == 54428 or spellID == 853 then -- Turn Evil, Divine Plea, Hammer of Justice
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Priest
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
+				elseif spellID == 10326 or spellID == 853 then -- Turn Evil, Hammer of Justice
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				elseif spellID == 9484 or spellID == 605 then -- Shackle Undead, Mind Control
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Rogue
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				elseif spellID == 6770 or spellID == 2094 or spellID == 51722 then -- Sap, Blind, Dismantle
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Shaman
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				elseif spellID == 51514 or spellID == 76780 then -- Hex, Bind Elemental
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Warlock
-				elseif spellID == 5484 or spellID == 710 or spellID == 5782 or spellID == 6358 then -- Howl of Terror, Banish, Fear, Seduction
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
-				-- Warrior
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
+				elseif spellID == 5484 or spellID == 710 or spellID == 5782 then -- Howl of Terror, Banish, Fear
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				elseif spellID == 676 or spellID == 85388 or spellID == 12809 then -- Disarm, Throwdown, Concussion Blow
-					SendChatMessage(GetSpellLink(spellID).." on "..destName..".", channel)
+					SendChatMessage(GetSpellLink(spellID).." on "..destName, channel)
 				end
-			elseif eventType == "SPELL_CAST_SUCCESS" then -- Conjured items
-				-- Shaman, Mage, Hunter
+			-- SPELL_AURA_APPLIED ended
+			elseif eventType == "SPELL_CAST_SUCCESS" then
 				if spellID == 32182 or spellID == 2825 or spellID == 80353 or spellID == 90355 then -- Bloodlust, Heroism, Timewarp, Ancient Hysteria
 					SendChatMessage("Sorry, misclicked", channel)
-				-- Warlock
 				elseif spellID == 29893 or spellID == 698 or spellID == 43987 then -- Ritual of Souls, Ritual of Summoning, Ritual of Refreshment
 					SendChatMessage(GetSpellLink(spellID)..", click it!", channel)
 				end
-			elseif eventType == "SPELL_AURA_REMOVED" and C.misc.ending == true then -- End of CCs
-				--if spellID == 
-					--SendChatMessage(GetSpellLink(spellID).." ended.", channel)
-			elseif eventType == "SPELL_SUMMON" and C.misc.conjured == true then -- Totems
-				-- Priest
+			-- SPELL_CAST_SUCCESS ended
+			elseif eventType == "SPELL_SUMMON" and C.misc.conjured == true then
 				if spellID == 724 then -- Lightwell
 					SendChatMessage(GetSpellLink(spellID).." up, click it!", channel)
-				-- Shaman
 				elseif spellID == 16190 or spellID == 98008 or spellID == 8143 then -- Mana Tide, Spirit Link, Tremor Totem
 					SendChatMessage(GetSpellLink(spellID).." up.", channel)
 				end
-			elseif event == "UNIT_DESTROYED" then
-				-- Shaman
-				if spellID == 16190 or spellID == 98008 or spellID == 8143 then -- Mana Tide, Spirit Link, Tremor Totem
-					SendChatMessage(GetSpellLink(spellID).." destroyed.", channel)
+			-- SPELL_SUMMON ended
+			elseif eventType == "SPELL_CAST_START" or eventType == "SPELL_CREATE" then
+				if spellID == 92649 or spellID == 92712 or spellID == 87915 or spellID == 87644 or spellID == 87643 then -- Cauldron of Battle, Big Cauldron of Battle, Goblin Barbecue, Seafood Magnifique, Broiled Dragon
+					SendChatMessage(GetSpellLink(spellID)..", eat!", channel)
 				end
+			-- SPELL_CAST_START and SPELL_CREATE ended
 			end
 		end
 	end
--- Tukui/modules/hoopla/yInterrupt.lua
+++ Tukui/modules/hoopla/yInterrupt.lua
@@ -3,15 +3,19 @@ local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, vari
 if C["misc"].interrupt ~= true then return end
 
 local function OnEvent(self, event, ...)
+if T.toc < 40200 then
+	_, eventType, _, _, sourceName, _, _, destName, _, spellID, spellName, _, extraskillID, extraskillname = ...
+else
+	_, eventType, _, _, sourceName, _, _, _, destName, _, _, spellID, spellName, _, extraskillID, extraskillname = ...
+end
 	if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
 		if GetNumRaidMembers() >= 5 then channel = "RAID" elseif GetNumPartyMembers() >= 1 then channel = "PARTY" else return end
-		local _, eventType, _, _, sourceName, _, _, destName, _, spellID, spellName, _, extraskillID, extraskillname = ...
+		--local channel = "SAY"
+		
 		if (eventType == "SPELL_INTERRUPT") and sourceName == T.myname then
 			SendChatMessage(GetSpellLink(extraskillID).." interrupted", channel)
 		elseif (eventType == "SPELL_STOLEN") and sourceName == T.myname then
 			SendChatMessage(GetSpellLink(extraskillID).." stolen.", channel)
-		elseif (eventType == "SPELL_PURGE") and sourceName == T.myname then
-			SendChatMessage(GetSpellLink(extraskillID).." removed.", channel)
 		end
 	end
 end
